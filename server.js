72c49a4c29c6074f20de2c360f0ec7899f662ec9
Yescii <cgonzalez.jesse@gmail.com>
2021-09-22 21:07:46 -0400

    added a database in models and exported to server.js to access it in a route

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const drinks = require("./models/drinks");
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Gitpub App!");
 });
 
+app.get("/drinks", (req, res) => {
+  res.send(drinks);
+});
 app.listen(port, () => {});
