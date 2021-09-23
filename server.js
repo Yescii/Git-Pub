6e0322c26b1cf771e12b298c11d86ba7cef44d31
Yescii <cgonzalez.jesse@gmail.com>
2021-09-22 23:42:24 -0400

    added a route to server.js to display what input is set in the route

--- server.js
+++ server.js
@@ -11,4 +11,8 @@ app.get("/drinks", (req, res) => {
   res.render("drinks_index.ejs", { drinks: drinks });
 });
 
+app.get("/drinks/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
 app.listen(port, () => {});
