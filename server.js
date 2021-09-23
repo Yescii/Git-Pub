04d07af19373cc250013410c6a8a109780c137c6
Yescii <cgonzalez.jesse@gmail.com>
2021-09-23 01:23:53 -0400

    added a database, food_index.ejs and food_show.ejs files. Updated the server.js file to render the routes for foods to be displayed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const drinks = require("./models/drinks");
+const foods = require("./models/food");
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Gitpub App!");
@@ -19,4 +20,11 @@ app.get("/drinks/:id", (req, res) => {
   res.render("drinks_show.ejs", { drink: drinks[req.params.id] });
 });
 
+app.get("/foods", (req, res) => {
+  res.render("food_index.ejs", { foods: foods });
+});
+
+app.get("/foods/:id", (req, res) => {
+  res.render("food_show.ejs", { food: foods[req.params.id] });
+});
 app.listen(port, () => {});
